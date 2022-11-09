6b92e891dc27a067345769dd68dc4095c2e68f43
daniloazeredo <33897652+daniloazeredo@users.noreply.github.com>
2022-11-09 10:37:26 -0300

    Initial commit

---
+++ Blazor-Excel-export/XLS/Excel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.JSInterop;
+using BlazorExcelExport.Models;
+
+namespace BlazorExcelExport.XLS;
+
+public class Excel
+{
+    public async Task GenerateWeatherForecastAsync(IJSRuntime js, 
+                                                   WeatherForecast[] data, 
+                                                   string filename = "export.xlsx")
+    {
+        var weatherForecast = new WeatherForecastXLS();
+        var XLSStream = weatherForecast.Edition(data);
+
+        await js.InvokeVoidAsync("BlazorDownloadFile", filename, XLSStream);
+    }
+
+    public async Task TemplateWeatherForecastAsync(IJSRuntime js, 
+                                                   Stream streamTemplate, 
+                                                   WeatherForecast[] data, 
+                                                   string filename = "export.xlsx")
+    {
+        var templateXLS = new UseTemplateXLS();
+		var XLSStream = templateXLS.Edition(streamTemplate, data);
+
+        await js.InvokeVoidAsync("BlazorDownloadFile", filename, XLSStream);
+    }
+
+
+    public async Task TemplateOnExistingFileAsync(HttpClient client, 
+                                                  IJSRuntime js, 
+                                                  Stream streamTemplate, 
+                                                  WeatherForecast[] data, 
+                                                  string existingFile)
+    {
+        var templateXLS = new UseTemplateXLS();
+        var XLSStream = await templateXLS.FillIn(client, streamTemplate, data, existingFile);
+        
+        await js.InvokeVoidAsync("BlazorDownloadFile", "export.xlsx", XLSStream);
+    }
+}
